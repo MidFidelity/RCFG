6382312af404b401e3f04df0fe1f7a2f3f8e2113
mk28 <trashqg1@gmail.com>
2019-09-20 22:47:14 +0200

    ADDED Mika

---
+++ RCFG/Assets/Mika/Scripts/ClickInterpreter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickInterpreter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camera;
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+
+            }
+        }
+    }
+}
---
+++ RCFG/Assets/Mika/Scripts/EventManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nvp.events
+{
+    public class NvpEventController
+    {
+        private static readonly Dictionary<string, GameEventWrapper> EventHandlers;
+
+        static NvpEventController()
+        {
+            EventHandlers = new Dictionary<string, GameEventWrapper>();
+        }
+
+        public static GameEventWrapper Events(string eventName)
+        {
+            if (!EventHandlers.ContainsKey(eventName))
+                EventHandlers[eventName] = new GameEventWrapper();
+
+            return EventHandlers[eventName];
+        }
+    }
+
+    public class GameEventWrapper
+    {
+        public event EventHandler GameEventHandler;
+
+        public void TriggerEvent(object sender, EventArgs eventArgs)
+        {
+            GameEventHandler?.Invoke(sender, eventArgs);
+        }
+    }
+
+    // Sample EventArgs
+    public class DefaultEventArgs : EventArgs
+    {
+        public object Value;
+    }
+}
