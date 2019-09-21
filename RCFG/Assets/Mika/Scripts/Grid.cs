deb8be1cf976eb328d81c644c5dbafe01dbaa2c0
Eli Kogan-Wang <elikoga@gmail.com>
2019-09-21 11:54:31 +0200

    Formatting

--- RCFG/Assets/Mika/Scripts/Grid.cs
+++ RCFG/Assets/Mika/Scripts/Grid.cs
@@ -6,15 +6,16 @@ using UnityEngine.Assertions;
 public class Grid : MonoBehaviour
 {
     public GameObject groundPrefab;
+
     public int gridWidth, gridHeight;
     // Start is called before the first frame update
     void Start()
     {
         Assert.IsNotNull(groundPrefab, "GroundPrefab not set");
 
-        for(int x = 0; x < gridWidth; x++)
+        for (int x = 0; x < gridWidth; x++)
         {
-            for(int y = 0; y < gridHeight; y++)
+            for (int y = 0; y < gridHeight; y++)
             {
                 Instantiate(groundPrefab, new Vector3(x, 0, y), Quaternion.identity, this.transform);
             }
@@ -25,6 +26,6 @@ public class Grid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
